3d9ae13afac24227096dbcb12de828a356dcce31
Heyson <hs@heysonseverino.com>
2020-12-19 20:24:19 +0000

    Add and style objects in api

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,11 +1,27 @@
-var xhr = new XMLHttpRequest();
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+function getData(type, cb) {
 
-xhr.send();
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this. responseText;
-    }
-};
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
+    xhr.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+           cb(JSON.parse(this.responseText));
+        }
+    };
+}
+    function writeToDocument(type) {
+        var el = document.getElementById("data");
+        el.innerHTML = "";
+        getData(type, function(data){
+            data = data.results;
+
+            data.forEach(function(item) {
+                 el.innerHTML += "<p>" + item.name + "</p>";
+            });
+            });      
+    };
+    
